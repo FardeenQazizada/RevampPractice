b4ef6e2dfdd408da53e83b028ad3d00141e3a89e
FardeenQazizada <133725844+FardeenQazizada@users.noreply.github.com>
2024-11-04 16:15:49 -0500

    Update PracticeOne.java
    second commit

--- src/main/java/practiceOne/PracticeOne.java
+++ src/main/java/practiceOne/PracticeOne.java
@@ -6,5 +6,6 @@ public class PracticeOne {
     public static void main(String[] args) {
 
         System.out.println("Coding is in my blood now");
+        System.out.println("You will be the toppest developers one day In Sha Allah!");
     }
 }
